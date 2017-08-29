b09db4ebbce49fabbb503717ea1f0c0ac8d7cef9
Ivette Guadalupe Hernández Pérez <ivette.hernandez@yaxche.mx>
2017-08-29 16:03:16 -0500

    Se agrega el Middleware

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 const reducer = (state, action) => {
     if (action.type === "LOG_IN") {
@@ -17,4 +17,11 @@ const reducer = (state, action) => {
     return state;
 };
 
-export default createStore(reducer, {user:""})
+const logger = store => next => action => {
+  console.log('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  return result
+}
+
+export default createStore(reducer, {user:""}, applyMiddleware(logger))
